8dff01152d7faa186258d8490588c72bad927826
Gustavo <guzanony1234@gmail.com>
2024-12-15 13:50:31 -0300

    Commit correto

--- listaUsuario-back-end/src/main/java/com/example/listaUsuario/Repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.listaUsuario.Repository;
-
-import com.example.listaUsuario.Entity.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
