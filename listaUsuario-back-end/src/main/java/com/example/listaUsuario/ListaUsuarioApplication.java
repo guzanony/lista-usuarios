d0c551f6347a7816f63b201b99d12d0b30877af8
Gustavo <guzanony1234@gmail.com>
2024-12-15 13:38:50 -0300

    Commit inicial

---
+++ listaUsuario-back-end/src/main/java/com/example/listaUsuario/ListaUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.example.listaUsuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaUsuarioApplication.class, args);
+	}
+
+}
---
+++ listaUsuario-back-end/src/test/java/com/example/listaUsuario/ListaUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.listaUsuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
